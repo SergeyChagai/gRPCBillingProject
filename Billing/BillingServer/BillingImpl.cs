091ae532295a9a14785f83e25c8ca31a0b0dfdd6
Chagaj Sergey <chagaj42@gmail.com>
2022-08-14 21:39:21 +0400

    Add server-side implementation of CoinsEmission & test it

--- Billing/BillingServer/BillingImpl.cs
+++ Billing/BillingServer/BillingImpl.cs
@@ -58,9 +58,9 @@ namespace Billing
         {
             lock (usersLock)
             {
-                var random = new Random();
                 var ratings = Users.Select(user => user.rating).ToArray();
                 var summaryRating = ratings.Sum();
+                var remainders = new Dictionary<int, long>();
 
                 foreach (JsonUserProfile jsonUser in Users)
                 {
@@ -68,14 +68,19 @@ namespace Billing
                     jsonUser.amount += 1;
                 }
 
-                foreach (JsonUserProfile jsonUser in Users)
+                for (int i = 0; i < Users.Count; i++)
                 {
+                    var jsonUser = Users[i];
                     var coinsForUser = amount * jsonUser.rating / summaryRating;
+                    remainders.Add(i, jsonUser.rating);
                     amount -= coinsForUser;
                     jsonUser.amount += coinsForUser;
                 }
-                var luckyIndex = random.Next(Users.Count);
-                Users[luckyIndex].amount += amount;
+                if (amount != 0)
+                {
+                    var idMax = remainders.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+                    Users[idMax].amount += amount;
+                }
             }
             return new Response { Status = Response.Types.Status.Ok };
         }
