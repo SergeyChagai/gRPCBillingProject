e304d59034dd8232d174c270d0c3826c932c28c3
Chagaj Sergey <chagaj42@gmail.com>
2022-08-15 18:10:19 +0400

    Refactoring, create tests

--- Billing/BillingServer/BillingImpl.cs
+++ Billing/BillingServer/BillingImpl.cs
@@ -9,10 +9,15 @@ namespace Billing
     public class BillingImpl : Billing.BillingBase
     {
         public List<JsonUserProfile> Users { get; private set; }
+        public List<Coin> Coins { get; private set; }
+
+        private const char Separator = ';';
+
         private readonly object usersLock = new object();
         public BillingImpl(List<JsonUserProfile> jsonUsers)
         {
             Users = new List<JsonUserProfile>(jsonUsers);
+            Coins = new List<Coin>();
         }
         public override async Task ListUsers(None request, IServerStreamWriter<UserProfile> responseStream, ServerCallContext context)
         {
@@ -23,35 +28,67 @@ namespace Billing
                     Name = user.name,
                     Amount = user.amount
                 };
-
                 await responseStream.WriteAsync(reply);
             }
         }
 
-        public override async Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
+        public override Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
         {
             var amount = request.Amount;
 
             if (amount < Users.Count)
             {
-                return new Response
+                return Task.FromResult(new Response
                 {
                     Status = Response.Types.Status.Failed,
                     Comment = "Coins amount is less of users"
-                };
+                });
             }
-            var response = await Task.Run(() => DistributeAmount(amount));
+            var response = Task.Run(() => DistributeAmount(amount));
             return response;
         }
 
         public override Task<Response> MoveCoins(MoveCoinsTransaction request, ServerCallContext context)
         {
-            return base.MoveCoins(request, context);
+            return Task.Run(() =>
+            {
+                lock (usersLock)
+                {
+                    var source = (Users.Find(i => i.name == request.SrcUser));
+                    var destination = (Users.Find(i => i.name == request.DstUser));
+                    var amount = request.Amount;
+
+                    if (source != null && destination != null && source.amount >= amount)
+                    {
+                        var sourceCoins = Coins.FindAll(c =>
+                        {
+                            var owners = c.History.Split(Separator);
+                            return owners.Last() == source.name;
+                        });
+                        ChangeOwnerOfCoins(sourceCoins, destination);
+                        source.amount -= amount;
+                        destination.amount += amount;
+                        return new Response { Status = Response.Types.Status.Ok };
+                    }
+                    return new Response { Status = Response.Types.Status.Failed };
+                }
+            });
         }
 
         public override Task<Coin> LongestHistoryCoin(None request, ServerCallContext context)
         {
-            return base.LongestHistoryCoin(request, context);
+            return Task.Run(() =>
+            {
+                return new Coin();
+            });
+        }
+
+        private void ChangeOwnerOfCoins(List<Coin> sourceCoins, JsonUserProfile destination)
+        {
+            foreach (Coin coin in sourceCoins)
+            {
+                coin.History = String.Join(Separator, coin.History, destination.name);
+            }
         }
 
         private Response DistributeAmount(long amount)
@@ -66,12 +103,14 @@ namespace Billing
                 {
                     amount -= 1;
                     jsonUser.amount += 1;
+                    CreateCoins(1, jsonUser.name);
                 }
 
                 for (int i = 0; i < Users.Count; i++)
                 {
                     var jsonUser = Users[i];
                     var coinsForUser = amount * jsonUser.rating / summaryRating;
+                    CreateCoins(coinsForUser, jsonUser.name);
                     remainders.Add(i, jsonUser.rating);
                     amount -= coinsForUser;
                     jsonUser.amount += coinsForUser;
@@ -79,10 +118,25 @@ namespace Billing
                 if (amount != 0)
                 {
                     var idMax = remainders.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+                    CreateCoins(amount, Users[idMax].name);
                     Users[idMax].amount += amount;
                 }
             }
             return new Response { Status = Response.Types.Status.Ok };
+        }
+
+        private void CreateCoins(long amount, string owner)
+        {
+            var lastUser = Coins.LastOrDefault();
+            var lastIndex = lastUser == null ? 0 : lastUser.Id;
+            for (int i = 0; i < amount; i++)
+            {
+                Coins.Add(new Coin
+                {
+                    Id = lastIndex + i + 1,
+                    History = owner
+                });
+            }
         }
     }
 }
--- Billing/BillingTests/BillingImplTests.cs
+++ Billing/BillingTests/BillingImplTests.cs
@@ -14,7 +14,7 @@ namespace BillingTests
 
         [Theory]
         [MemberData(nameof(CoinsEmissionTestData))]
-        public async Task CoinsEmissionTest(long amount, List<UserProfile> expected)
+        public async Task CoinsEmissionTest(long amount, List<UserProfile> expected, List<Coin> expectedCoins)
         {
             //Arrange
             sut = new BillingImpl(BillingUtil.LoadUsers());
@@ -25,10 +25,16 @@ namespace BillingTests
             {
                 actual.Add(new UserProfile { Name = jsonUser.name, Amount = jsonUser.amount });
             }
+            var actualCoins = sut.Coins;
 
             //Assert
             Assert.True(response.Status == Response.Types.Status.Ok);
             Assert.Equal(expected, actual);
+            Assert.Equal(expectedCoins.Count, actualCoins.Count);
+            for (int i = 0; i < expectedCoins.Count; i++)
+            {
+                Assert.Contains(actualCoins, c => c.History == expectedCoins[i].History);
+            }
         }
 
         [Theory]
@@ -44,6 +50,109 @@ namespace BillingTests
 
             //Assert
             Assert.True(response.Status == Response.Types.Status.Failed);
+        }
+
+        [Theory]
+        [MemberData(nameof(MoveCoinsTestData))]
+        public async Task MoveCoinsTest(MoveCoinsTransaction request, List<UserProfile> expected)
+        {
+            //Arrange
+            sut = new BillingImpl(BillingUtil.LoadUsers());
+            await sut.CoinsEmission(new EmissionAmount { Amount = 1000 }, null);
+
+            //Act
+            var response = await sut.MoveCoins(request, null);
+            var actual = new List<UserProfile>();
+            foreach (JsonUserProfile jsonUser in sut.Users)
+            {
+                actual.Add(new UserProfile { Name = jsonUser.name, Amount = jsonUser.amount });
+            }
+
+            //Assert
+            Assert.Equal(Response.Types.Status.Ok, response.Status);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(LongestHistoryCoinTestData))]
+        public async Task LongestHistoryCoinTest(MoveCoinsTransaction[] transactions, Coin expected)
+        {
+            //Arrange
+            sut = new BillingImpl(BillingUtil.LoadUsers());
+            await sut.CoinsEmission(new EmissionAmount { Amount = 1000 }, null);
+            foreach (MoveCoinsTransaction transaction in transactions)
+            {
+                await sut.MoveCoins(transaction, null);
+            }
+
+            //Act
+            var actual = await sut.LongestHistoryCoin(new None(), null);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        public static IEnumerable<object[]> LongestHistoryCoinTestData()
+        {
+            yield return new object[]
+            {
+                new MoveCoinsTransaction[]
+                {
+                    new MoveCoinsTransaction { SrcUser = "boris", DstUser = "maria", Amount = 1 },
+                    new MoveCoinsTransaction { SrcUser = "boris", DstUser = "oleg", Amount = 1 },
+                    new MoveCoinsTransaction { SrcUser = "maria", DstUser = "oleg", Amount = 1 },
+                },
+                new Coin{ Id = 1, History = "boris;maria;oleg" }
+            };
+        }
+        public static IEnumerable<object[]> MoveCoinsTestData()
+        {
+            yield return new object[]
+            {
+                new MoveCoinsTransaction { SrcUser = "boris", DstUser = "maria", Amount = 100 },
+                new List<UserProfile>
+                {
+                    new UserProfile
+                    {
+                        Name = "boris",
+                        Amount = 834
+                    },
+                     new UserProfile
+                    {
+                        Name = "maria",
+                        Amount = 139
+                    },
+                      new UserProfile
+                    {
+                        Name = "oleg",
+                        Amount = 27
+                    }
+            }
+        };
+            yield return new object[]
+            {
+            new MoveCoinsTransaction { SrcUser = "boris", DstUser = "oleg", Amount = 50 },
+             new List<UserProfile>
+            {
+                new UserProfile
+                {
+                    Name = "boris",
+                    Amount = 884
+                },
+                 new UserProfile
+                {
+                    Name = "maria",
+                    Amount = 39
+                },
+                  new UserProfile
+                {
+                    Name = "oleg",
+                    Amount = 77
+                }
+            }
+        };
         }
 
         public static IEnumerable<object[]> CoinsEmissionTestData()
@@ -66,7 +175,14 @@ namespace BillingTests
                     Amount = 1
                 }
             };
-            yield return new object[] { 3, users };
+            var coins = new List<Coin>
+            {
+                new Coin{Id = 1, History = "boris"},
+                new Coin{Id = 2, History = "maria"},
+                new Coin{Id = 3, History = "oleg"},
+
+            };
+            yield return new object[] { 3, users, coins };
 
             users = new List<UserProfile>
             {
@@ -86,7 +202,17 @@ namespace BillingTests
                     Amount = 1
                 }
             };
-            yield return new object[] { 6, users };
+            coins = new List<Coin>
+            {
+                new Coin{Id = 1, History = "boris"},
+                new Coin{Id = 2, History = "boris"},
+                new Coin{Id = 3, History = "boris"},
+                new Coin{Id = 4, History = "boris"},
+                new Coin{Id = 5, History = "maria"},
+                new Coin{Id = 6, History = "oleg"},
+
+            };
+            yield return new object[] { 6, users, coins };
 
         }
     }
